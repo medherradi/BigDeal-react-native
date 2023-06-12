ab43e22aa94b0a0a4b2b4fb6c66add0a8fc92e48
medherradi <med.herradi@gmail.com>
2023-06-12 18:16:27 +0000

    clear all ...

--- app/components/AppImagePicker.jsx
+++ app/components/AppImagePicker.jsx
@@ -4,12 +4,17 @@ import AppScreen from './AppScreen'
 import * as ImagePicker from 'expo-image-picker';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import colors from '../config/colors';
+import AppText from './AppText';
 
 export default function AppImagePicker() {
   const [images, setImages] = useState([])
 
   const handlImage = async () => {
-    const result = await ImagePicker.launchImageLibraryAsync()
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+    }
+    )
     if (!result.canceled) {
       console.log(result.assets[0])
       setImages((prev) => [...prev, result.assets[0].uri])
@@ -19,31 +24,50 @@ export default function AppImagePicker() {
 
 
   return (
+
     <AppScreen>
-      <ScrollView horizontal>
-        {images.length ?
-          images.map((image) => {
-            return <View key={image} style={styles.imgcont} >
-              <Image style={styles.img} source={{ uri: image }} />
-              <TouchableHighlight onPress={() => setImages(images.filter((img) => img !== image))} underlayColor={colors.light}>
-                <View style={styles.icon}>
-                  <MaterialCommunityIcons name="trash-can" size={20} color={colors.layer} />
-                </View>
-              </TouchableHighlight>
+      <View style={styles.scroll}>
+        <ScrollView horizontal>
+          {images.length ?
+            images.map((image) => {
+              return <View key={image} style={styles.imgcont} >
+                <Image style={styles.img} source={{ uri: image }} />
+                <TouchableHighlight onPress={() => setImages(images.filter((img) => img !== image))} underlayColor={colors.light}>
+                  <View style={styles.icon}>
+                    <MaterialCommunityIcons name="trash-can" size={20} color={colors.layer} />
+                  </View>
+                </TouchableHighlight>
+              </View>
+            }) : null
+          }
+          <TouchableHighlight style={styles.button} onPress={handlImage} underlayColor={colors.light} >
+            <View style={styles.container}>
+              <MaterialCommunityIcons name="camera" size={40} color={colors.layer} />
             </View>
-          }) : null
-        }
-        <TouchableHighlight onPress={handlImage} underlayColor={colors.light} >
-          <View style={styles.container}>
-            <MaterialCommunityIcons name="camera" size={40} color={colors.layer} />
-          </View>
+          </TouchableHighlight>
+        </ScrollView>
+      </View>
+      {(images.length > 1) ? <View style={styles.clearButton}>
+        <TouchableHighlight style={styles.clear} onPress={() => setImages([])} underlayColor={colors.light} >
+          <AppText style={styles.text}>clear all</AppText>
         </TouchableHighlight>
-      </ScrollView>
+      </View> : null}
     </AppScreen >
+
   )
 }
 
 const styles = StyleSheet.create({
+  scroll: {
+    flexDirection: 'row',
+    height: 100,
+  },
+  button: {
+    flex: 1,
+    flexDirection: 'row',
+    width: 100,
+    height: 100,
+  },
   container: {
     width: 100,
     height: 100,
@@ -73,5 +97,22 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 5,
     right: 5
+  },
+  clear: {
+    marginTop: 10,
+    width: 150,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.white
+  },
+  text: {
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+  clearButton: {
+    width: '100%',
+    alignItems: 'center'
   }
 })
